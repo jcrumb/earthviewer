dd9a126aa739a24ce749d8d83e4dcef5e6dc5b75
Jay Crumb <jay@toxicedge.com>
2015-09-07 15:29:10 -0400

    Initial commit of project files

---
+++ LibEarthView/EarthView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Windows.Media.Imaging;
+
+namespace EarthView
+{
+    public class EarthView
+    {
+        public string URL { get; set; }
+
+        public EarthView(string url)
+        {
+            this.URL = url;
+        }
+
+        public ImageSource GetImage()
+        {
+            var webClient = new WebClient();
+            var bytes = webClient.DownloadData(this.URL);
+
+            MemoryStream ms = new MemoryStream(bytes);
+
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.StreamSource = ms;
+            bitmap.EndInit();
+
+            return bitmap as ImageSource;
+        }
+    }
+}
